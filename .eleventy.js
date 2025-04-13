e65da2b65fa638cc7d940c5610edef0218918699
Maneeshwar <maneeshwarkarne@gmail.com>
2025-04-13 20:51:32 +0530

    CMS addition

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     // tell to get it css . if eleventy doesnt pass thruough something from src to public. you gotta tell it explicitly to do so.
     eleventyConfig.addPassthroughCopy('./src/style.css')
     eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/admin')
     
     eleventyConfig.addFilter("postDate", (dateObj)=>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
